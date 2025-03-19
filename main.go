8af02bc3c41b862710459fd0bea5b9706ad44e61
slickmorty <ad.abasi1997@gmail.com>
2025-03-19 17:54:23 +0330

    fib

--- main.go
+++ main.go
@@ -4,23 +4,47 @@ import "fmt"
 
 func main() {
 
-	a := 10
-	b := 20
-
-	a, b = swap(a, b)
-
-	fmt.Println("a is ", a)
-	fmt.Println("b is ", b)
-}
-
-func swap(a int, b int) (int, int) {
-
-	var c int
-
-	c = a
-	a = b
-	b = c
-
-	return a, b
+	fmt.Println(fib(6))
+	fmt.Println(fib2(6))
 
 }
+
+func fib(a int) int {
+
+	if a == 1 || a == 2 {
+		return 1
+	}
+
+	return fib(a-1) + fib(a-2)
+}
+
+func fib2(a int) int {
+
+	s := 0
+	f1 := 0
+	f2 := 0
+
+	for i := 1; i < a; i++ {
+
+		if i <= 2 {
+			f1 = 1
+			f2 = 1
+			s = 1
+		}
+
+		s = f1 + f2
+		f1 = f2
+		f2 = s
+
+	}
+
+	return s
+
+}
+
+/*
+
+	1 1 2 3 5 8 13 21 34
+
+
+*/
