727ebaae90ff858da07aa71e944ef5166e235a80
slickmorty <ad.abasi1997@gmail.com>
2025-03-20 16:41:02 +0330

    pallindrum

--- main.go
+++ main.go
@@ -4,49 +4,15 @@ import "fmt"
 
 func main() {
 
-	factorial(4)
+	some := "hosein"
 
-	fmt.Println(factorial2(4))
-
-}
-
-func factorial(a int) int {
-
-	if a <= 1 {
-		return 1
-	}
-	s := 1
-
-	for i := a; i >= 1; i-- {
-		s = s * i
-	}
-
-	fmt.Println(s)
-
-	return s
-}
-
-func factorial2(a int) int {
-
-	if a == 1 {
-		return 1
-	}
-
-	return a * factorial2(a-1)
+	fmt.Println(string(some[len(some)-1]))
 
 }
 
 /*
 
-	1,1,2,3,5,8,13,21,...
-
-	fib(1) = 1
-	fib(2) = 1
-
-	for n >= 3{
-
-		fib(n) = fib(n-1) + fib(n-2)
-	}
-
+	AsssA -> True
+	AssaA -> False
 
 */
