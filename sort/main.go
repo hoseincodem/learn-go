f426aa7dfe213097111ed46f4cfe8196b72ead73
slickmorty <ad.abasi1997@gmail.com>
2025-03-22 16:56:58 +0330

    sorting stuff

--- sort/main.go
+++ sort/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"slices"
 )
 
 func main() {
@@ -13,36 +14,71 @@ func main() {
 		numbers[i] = rand.Intn(100)
 	}
 	fmt.Println(numbers)
-	fmt.Println(min(numbers))
-	fmt.Println(max(numbers))
+
+	fmt.Println(sort(numbers))
 
 }
 
-func min(n []int) int {
+func sort(n []int) []int {
+
+	b := make([]int, 0)
+
+	lenOfSlice := len(n)
+
+	for i := 0; i < lenOfSlice; i++ {
+
+		minNumber, minIndex := minWithIndex(n)
+
+		b = append(b, minNumber)
+
+		n = slices.Delete(n, minIndex, minIndex+1)
+
+	}
+
+	return b
+}
+
+func minWithIndex(n []int) (int, int) {
 
 	minNumber := n[0]
+	minIndex := 0
 
 	for i := 0; i < len(n); i++ {
 
 		if n[i] < minNumber {
 			minNumber = n[i]
+			minIndex = i
 		}
-
 	}
 
-	return minNumber
+	return minNumber, minIndex
 }
 
-func max(n []int) int {
+// func min(n []int) int {
 
-	MaxNumber := n[0]
+// 	minNumber := n[0]
 
-	for i := 0; i < len(n); i++ {
+// 	for i := 0; i < len(n); i++ {
 
-		if n[i] > MaxNumber {
-			MaxNumber = n[i]
-		}
+// 		if n[i] < minNumber {
+// 			minNumber = n[i]
+// 		}
 
-	}
-	return MaxNumber
-}
+// 	}
+
+// 	return minNumber
+// }
+
+// func max(n []int) int {
+
+// 	MaxNumber := n[0]
+
+// 	for i := 0; i < len(n); i++ {
+
+// 		if n[i] > MaxNumber {
+// 			MaxNumber = n[i]
+// 		}
+
+// 	}
+// 	return MaxNumber
+// }
