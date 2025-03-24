92a7eed8b671d5a96b40073f7f3720e80d4b5ec3
slickmorty <ad.abasi1997@gmail.com>
2025-03-24 17:05:36 +0330

    bubble sort

---
+++ sort/bubble_sort/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	numbers := make([]int, 50)
+
+	for i := 0; i < len(numbers); i++ {
+		numbers[i] = rand.Intn(100)
+	}
+	fmt.Println(numbers)
+
+	fmt.Println(bubbleSort(numbers))
+
+}
+
+func bubbleSort(n []int) []int {
+
+	for i := 0; i < len(n); i++ {
+
+		for j := 0; j < len(n)-1; j++ {
+
+			if n[j] > n[j+1] {
+				n[j], n[j+1] = n[j+1], n[j]
+			}
+
+		}
+
+	}
+
+	return n
+}
