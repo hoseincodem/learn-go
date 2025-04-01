a8527942edd015944533bbc0ed228f6dafe9321e
slickmorty <ad.abasi1997@gmail.com>
2025-04-01 16:45:16 +0330

    dobare rid

--- time_sort/main.go
+++ time_sort/main.go
@@ -20,25 +20,11 @@ func main() {
 
 	// Sort these times
 
-	sort(randomTimes)
+	// sort(randomTimes)
 
 	fmt.Println("Sorted :")
-	for _, t := range randomTimes {
+	for _, t := range sortTime(randomTimes) {
 		fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute())
-	}
-}
-func sort(times []time.Time) {
-
-	for j := 0; j < len(times); j++ {
-
-		for i := 0; i < len(times)-1-j; i++ {
-
-			if times[i].Year() > (times[i+1]).Year() && times[i].Minute() > (int(times[i+1].Month())) && times[i].Day() > (int(times[i+1].Day())) && times[i].Minute() > (int(times[i+1].Minute())) {
-
-				times[i], times[i+1] = times[i+1], times[i]
-
-			}
-		}
 	}
 }
 
@@ -54,4 +40,88 @@ func generateRandomTimes(n int, start time.Time, end time.Time) []time.Time {
 	}
 
 	return times
+}
+
+// SORT TIMES
+func sortTime(times []time.Time) []time.Time {
+
+	timeMap := map[int]time.Time{}
+
+	intTimes := []int{}
+
+	for _, t := range times {
+
+		intTime := convertTimeToInt(t)
+
+		timeMap[intTime] = t
+
+		intTimes = append(intTimes, intTime)
+	}
+
+	for i := 0; i < len(intTimes); i++ {
+
+		for j := i + 1; j < len(intTimes); j++ {
+
+			if intTimes[i] > intTimes[j] {
+				intTimes[i], intTimes[j] = intTimes[j], intTimes[i]
+			}
+		}
+	}
+
+	for i := range times {
+
+		times[i] = timeMap[intTimes[i]]
+
+	}
+
+	return times
+
+}
+
+// CONVERT TIME TO INT
+func convertTimeToInt(t time.Time) int {
+
+	year := t.Year()
+	monthString := t.Month().String()
+	day := t.Day()
+	hour := t.Hour()
+	minute := t.Minute()
+	second := t.Second()
+
+	m := map[string]int{
+		"January":   1,
+		"February":  2,
+		"March":     3,
+		"April":     4,
+		"May":       5,
+		"June":      6,
+		"July":      7,
+		"August":    8,
+		"September": 9,
+		"October":   10,
+		"November":  11,
+		"December":  12,
+	}
+
+	month := m[monthString]
+
+	var a int
+
+	a += year
+	a = a * 100
+	a = a + month
+
+	a = a * 100
+	a = a + day
+
+	a = a * 100
+	a = a + hour
+
+	a = a * 100
+	a = a + minute
+
+	a = a * 100
+	a = a + second
+
+	return a
 }
